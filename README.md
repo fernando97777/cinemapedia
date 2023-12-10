c079b9ae2d14c30c60863cb5cf3e463648e082f7
fervelez977 <fervelez977@gmail.com>
2023-12-09 19:34:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cinemapedia
+
+# dev
+1. Copiar el .env.template y renombrar a .env
+2. Cambiar las variables de entorno (The MovieDB)
---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  //ruta del screen con go_router
+  static const name = "home-screen";
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
